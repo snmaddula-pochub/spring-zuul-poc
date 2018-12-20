2c6644b5bc9516cc43eb1df544fd61eecac88465
snmaddula <snmaddula@outlook.com>
2018-12-21 05:09:20 +0530

    Initial Commit

---
+++ src/main/java/snmaddula/pochub/zuul/SpringZuulPocApplication.java
@@ -0,0 +1,14 @@
+package snmaddula.pochub.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZuulPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZuulPocApplication.class, args);
+	}
+
+}
+
